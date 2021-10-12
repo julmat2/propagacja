7ee7ff7fa89123ee2ee6cdfe3e6e983446040fe9
julmat2 <matysiakjulia@gmail.com>
2021-10-12 19:29:56 +0200

    wczytywanie plików, dodany model grawitacji oraz oddziaływanie słońca i księżyca, wczytywanie plików konfiguracyjnych

---
+++ src/main/java/test_kompilatora.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class test_kompilatora {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
